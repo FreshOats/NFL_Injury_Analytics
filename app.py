e1ed3935e55dc16e03f896f066da7f11c5f91381
Justin Papreck <justin.papreck@gmail.com>
2022-11-17 17:57:22 -0800

    working scrape, not ideal

--- app.py
+++ app.py
@@ -1,26 +1,44 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_pymongo import PyMongo
-import scraping
+from flask import (
+   Flask, 
+   render_template, 
+   request,  
+   flash
+)
 
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup, SoupStrainer
+import requests
+import pandas as pd
+
+
+
+# Create the Flask Instance
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/injuries_app"
-mongo = PyMongo(app)
 
-
-@app.route("/")
+@app.route("/", methods=("GET", "POST"), strict_slashes=False)
 def index():
-   injuries = mongo.db.injuries.find_one()
-   return render_template("index.html", injuries=injuries)
+   # Parsing Code will go here
+   if request.method == "POST":
+      try:
+         global url, specific_element
 
+         url = "https://www.cbssports.com/nfl/injuries/daily"
 
-@app.route("/scrape")
-def scrape():
-   injuries = mongo.db.injuries
-   injury_data = scraping.scrape_injuries()
-   injuries.update_one({}, {"$set": injury_data}, upsert=True)
-   return redirect('/', code=302)
+         only_tr = SoupStrainer('tr')
+         source = requests.get(url).text
+         soup = BeautifulSoup(source, 'lxml', parse_only=only_tr)
+         specific_element = soup.find_all('tr', limit=16)[1:]
+ 
+
+         return render_template("index.html",
+                                results=specific_element
+                                )
+
+      except Exception as e:
+         flash(e, 'danger')
+
+   return render_template('index.html')
 
 
 if __name__ == "__main__":
