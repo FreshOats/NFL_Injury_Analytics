c677eb2ab717d28e9114e0859392601d43b520ab
Justin Papreck <justin.papreck@gmail.com>
2022-11-29 19:46:18 -0800

    Fixed SQL QUery

--- SQL/Punt_File_Merger.sql
+++ SQL/Punt_File_Merger.sql
@@ -1,22 +1,28 @@
+DROP TABLE IF EXISTS punt_analytics;
+
+
 CREATE TABLE punt_analytics AS
-SELECT
-	ppr.gamekey, 
-	ppr.playid, 
+SELECT ppr.gamekey,
+	ppr.playid,
 	ppr.gsisid,
-	pi.season_type, 
-	pi.quarter, 
-	pi.score_home_visiting, 
-	gms.stadiumtype, 
-	gms.turf, 
-	gms.gameweather, 
-	gms.temperature, 
+	ppr.prole,
+	pi.season_type,
+	pi.quarter,
+	pi.score_home_visiting,
+	gms.stadiumtype,
+	gms.turf,
+	gms.week,
+	gms.gameweather,
+	gms.temperature,
 	pp.p_position
 FROM play_player_role AS ppr
-INNER JOIN play_info AS pi
-	ON (ppr.gamekey = pi.gamekey AND  ppr.playid = pi.playid)
-LEFT JOIN game_data AS gms
-	ON (ppr.gamekey = gms.gamekey)
-LEFT JOIN player_punt_data AS pp
-	ON (ppr.gsisid = pp.gsisid)
-;
+INNER JOIN play_info AS pi ON (ppr.gamekey = pi.gamekey
+AND ppr.playid = pi.playid)
+LEFT JOIN game_data AS gms ON (ppr.gamekey = gms.gamekey)
+LEFT JOIN player_punt_data AS pp ON (ppr.gsisid = pp.gsisid) ;
+
+
+SELECT *
+FROM punt_analytics
+LIMIT 10;
 
